64b45585449b718c69ba61775c5c7dcbf0694f77
silverjimmy <silverjimmy2@gmail.com>
2018-02-22 09:14:51 +0300

    Enable cors

--- run.py
+++ run.py
@@ -3,7 +3,9 @@ import sys
 from app import create_app
 
 config_name = os.getenv('ENV')
-app = create_app("development")
-
+app = create_app("production")
+response.headers.add('Access-Control-Allow-Origin', '*')
+response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
 if __name__ == '__main__':
     app.run()
