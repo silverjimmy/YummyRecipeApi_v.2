99e2b80e0e374ef21a6ec419e80e04571e5c8af7
silverjimmy <silverjimmy2@gmail.com>
2018-02-22 10:04:24 +0300

    Enable cors

--- run.py
+++ run.py
@@ -4,8 +4,12 @@ from app import create_app
 
 config_name = os.getenv('ENV')
 app = create_app("production")
-response.headers.add('Access-Control-Allow-Origin', '*')
-response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+
+@blueprint.after_request # blueprint can also be app~~
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    return response
+
 if __name__ == '__main__':
     app.run()
