75a88dbdcb1352650f825270f5099a5ce51ae813
silverjimmy <silverjimmy2@gmail.com>
2018-02-22 10:58:38 +0300

    Enabling cors

--- run.py
+++ run.py
@@ -2,23 +2,9 @@ import os
 import sys
 from app import create_app
 
-app = create_app("production")
-app.use((req, res, next) => {
-  const origin = req.get('origin');
-
-  // TODO Add origin validation
-  res.header('Access-Control-Allow-Origin', origin);
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
-
-  // intercept OPTIONS method
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(204);
-  } else {
-    next();
-  }
-});
+config_name = os.getenv('ENV')
+app = create_app("development")
+request.headers['Access-Control-Allow-Origin'] = '*'
 
 if __name__ == '__main__':
     app.run()
