8d595c9bda758af0ca6b0c453b09f550919a7e11
Herbert Bruno <herbert.oketayot@andela.com>
2018-12-12 12:59:55 +0300

    set max of 100 char for email

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True)
     password = db.Column(db.String(25))
-    email = db.Column(db.String(25), unique=True)
+    email = db.Column(db.String(100), unique=True)
     category = db.relationship('Category', backref='user',
                                   cascade='all, delete',
                                   lazy='dynamic')
